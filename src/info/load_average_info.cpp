2ae25cc381653b576b0879d15fd047a5ec8cacba
Mitch Waibel <mdub555@gmail.com>
2018-02-21 16:18:22 -0700

    Implemented load_average_info.cpp.

--- src/info/load_average_info.cpp
+++ src/info/load_average_info.cpp
@@ -7,5 +7,18 @@ using namespace std;
 
 LoadAverageInfo get_load_average() {
   // TODO: implement me
-  return LoadAverageInfo();
+  ifstream loadavg_file(PROC_ROOT "/loadavg");
+
+  if (!loadavg_file) {
+    cerr << "Unable to read from /proc/loadavg" << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  LoadAverageInfo info;
+
+  loadavg_file
+    >> info.one_min
+    >> info.five_mins
+    >> info.fifteen_mins;
+  return info;
 }
